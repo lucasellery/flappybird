2e04e50c6d3aee4e432deb4c990d2f15846c202a
Your Name <olucasellery@gmail.com>
2020-08-10 21:46:40 -0300

    Adicionando troca de telas

--- jogo.js
+++ jogo.js
@@ -8,94 +8,155 @@ const contexto = canvas.getContext('2d'); // define jogo com 2D
 
 // Background
 const planoDeFundo = {
-    spriteX: 390,
-    spriteY: 0,
-    largura: 275,
-    altura: 204,
-    x: 0,
-    y: canvas.height - 204,
-    desenha() {
+  spriteX: 390,
+  spriteY: 0,
+  largura: 275,
+  altura: 204,
+  x: 0,
+  y: canvas.height - 204,
+  desenha() {
 
-        contexto.fillStyle = '#70c5ce';
-        contexto.fillRect(0, 0, canvas.width, canvas.height);
+    contexto.fillStyle = '#70c5ce';
+    contexto.fillRect(0, 0, canvas.width, canvas.height);
 
-        contexto.drawImage(
-            sprites,
-            planoDeFundo.spriteX, planoDeFundo.spriteY, // sprite x e sprite y
-            planoDeFundo.largura, planoDeFundo.altura, // tamanho do recorte na sprite
-            planoDeFundo.x, planoDeFundo.y,
-            planoDeFundo.largura, planoDeFundo.altura
-        );
+    contexto.drawImage(
+      sprites,
+      planoDeFundo.spriteX, planoDeFundo.spriteY, // sprite x e sprite y
+      planoDeFundo.largura, planoDeFundo.altura, // tamanho do recorte na sprite
+      planoDeFundo.x, planoDeFundo.y,
+      planoDeFundo.largura, planoDeFundo.altura
+    );
 
-        contexto.drawImage(
-            sprites,
-            planoDeFundo.spriteX, planoDeFundo.spriteY, // sprite x e sprite y
-            planoDeFundo.largura, planoDeFundo.altura, // tamanho do recorte na sprite
-            (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
-            planoDeFundo.largura, planoDeFundo.altura
-        );
-    }
+    contexto.drawImage(
+      sprites,
+      planoDeFundo.spriteX, planoDeFundo.spriteY, // sprite x e sprite y
+      planoDeFundo.largura, planoDeFundo.altura, // tamanho do recorte na sprite
+      (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
+      planoDeFundo.largura, planoDeFundo.altura
+    );
+  }
 };
 
 const chao = {
-    spriteX: 0,
-    spriteY: 610,
-    largura: 224,
-    altura: 112,
-    x: 0,
-    y: canvas.height - 112,
-    desenha() {
-        contexto.drawImage(
-            sprites,
-            chao.spriteX, chao.spriteY, // sprite x e sprite y
-            chao.largura, chao.altura, // tamanho do recorte na sprite
-            chao.x, chao.y,
-            chao.largura, chao.altura
-        );
+  spriteX: 0,
+  spriteY: 610,
+  largura: 224,
+  altura: 112,
+  x: 0,
+  y: canvas.height - 112,
+  desenha() {
+    contexto.drawImage(
+      sprites,
+      chao.spriteX, chao.spriteY, // sprite x e sprite y
+      chao.largura, chao.altura, // tamanho do recorte na sprite
+      chao.x, chao.y,
+      chao.largura, chao.altura
+    );
 
-        contexto.drawImage(
-            sprites,
-            chao.spriteX, chao.spriteY, // sprite x e sprite y
-            chao.largura, chao.altura, // tamanho do recorte na sprite
-            (chao.x + chao.largura), chao.y,
-            chao.largura, chao.altura
-        );
-    }
+    contexto.drawImage(
+      sprites,
+      chao.spriteX, chao.spriteY, // sprite x e sprite y
+      chao.largura, chao.altura, // tamanho do recorte na sprite
+      (chao.x + chao.largura), chao.y,
+      chao.largura, chao.altura
+    );
+  }
 };
 
 const flappyBird = { // estrutura que representa o passaro
-    spriteX: 0,
-    spriteY: 0,
-    largura: 33,
-    altura: 24,
-    x: 10,
-    y: 50,
-    gravidade: 0.25,
-    velocidade: 0,
-    atualiza() {
-        flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
-        flappyBird.y = flappyBird.y + flappyBird.velocidade;
-    },
-    desenha() {
-        contexto.drawImage(
-            sprites,
-            flappyBird.spriteX, flappyBird.spriteY, // sprite x e sprite y
-            flappyBird.largura, flappyBird.altura, // tamanho do recorte na sprite
-            flappyBird.x, flappyBird.y,
-            flappyBird.largura, flappyBird.altura
-        );
-    }
+  spriteX: 0,
+  spriteY: 0,
+  largura: 33,
+  altura: 24,
+  x: 10,
+  y: 50,
+  gravidade: 0.25,
+  velocidade: 0,
+  atualiza() {
+    flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
+    flappyBird.y = flappyBird.y + flappyBird.velocidade;
+  },
+  desenha() {
+    contexto.drawImage(
+      sprites,
+      flappyBird.spriteX, flappyBird.spriteY, // sprite x e sprite y
+      flappyBird.largura, flappyBird.altura, // tamanho do recorte na sprite
+      flappyBird.x, flappyBird.y,
+      flappyBird.largura, flappyBird.altura
+    );
+  }
 }
 
+const mensagemGetReady = { // estrutura que representa o passaro
+  spriteX: 134,
+  spriteY: 0,
+  largura: 174,
+  altura: 152,
+  x: (canvas.width / 2) - 174 / 2,
+  y: 50,
+  desenha() {
+    contexto.drawImage(
+      sprites,
+      mensagemGetReady.spriteX, mensagemGetReady.spriteY, // sprite x e sprite y
+      mensagemGetReady.largura, mensagemGetReady.altura, // tamanho do recorte na sprite
+      mensagemGetReady.x, mensagemGetReady.y,
+      mensagemGetReady.largura, mensagemGetReady.altura
+    );
+  }
+}
 
-function loop() {
-    flappyBird.atualiza();
+/**
+ *  Telas
+ */
+
+let telaAtiva = {};
+function mudaParaTela(novaTela) {
+  telaAtiva = novaTela;
+}
+
+const Telas = {
+  INICIO: {
+    desenha() {
+      planoDeFundo.desenha();
+      chao.desenha();
+      flappyBird.desenha();
+      mensagemGetReady.desenha();
+    },
+    click() {
+      mudaParaTela(Telas.JOGO);
+    },
+
+    atualiza() {
+
+    }
+  }
+};
+
+Telas.JOGO = {
+  desenha() {
     planoDeFundo.desenha();
     chao.desenha();
     flappyBird.desenha();
-
-
-    requestAnimationFrame(loop);
+  },
+  atualiza() {
+    flappyBird.atualiza();
+  }
 }
 
+function loop() {
+
+  telaAtiva.desenha();
+  telaAtiva.atualiza();
+
+  requestAnimationFrame(loop);
+}
+
+window.addEventListener('click', function () {
+  if (telaAtiva.click) {
+    telaAtiva.click();
+  }
+
+});
+
+mudaParaTela(Telas.INICIO);
 loop();
